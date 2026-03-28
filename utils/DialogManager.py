97ee7196a42d1d6e225d188946bcb0a1d8347d74
SergeMaikere <serge.maikere@gmail.com>
2026-03-28 17:40:46 +0100

    + Timer 500 ms between each dialog lines

--- utils/DialogManager.py
+++ utils/DialogManager.py
@@ -4,6 +4,7 @@ from gameobj.Dialog import Dialog
 from settings import *
 from entities.Player import Player
 from utils.AllSprites import AllSprites
+from utils.Timer import Timer
 
 class DialogManager:
 	def __init__ ( self, player: Player, characters: Group, all_sprites: AllSprites ):
@@ -11,18 +12,21 @@ class DialogManager:
 		self.player = player
 		self.characters = characters
 		self.all_sprites = all_sprites
+
+		self.timer = Timer(500)
 		self.current_dialog = None
 		
 
 	def input ( self ):
-		keys = pygame.key.get_just_pressed()
-		if keys[pygame.K_SPACE]:
-			for character in self.characters:
-				if self.__is_dialog_available(self.player, character):
-					if self.current_dialog: 
-						self.current_dialog.update()
-					else:
-						self.current_dialog = Dialog(character, self.finish_dialog, self.all_sprites)
+			keys = pygame.key.get_just_pressed()
+			if keys[pygame.K_SPACE]:
+				for character in self.characters:
+					if self.__is_dialog_possible(self.player, character):
+						if self.current_dialog: 
+							self.current_dialog.update()
+						else:
+							self.current_dialog = Dialog(character, self.finish_dialog, self.all_sprites)
+						self.timer.start()
 
 	def __is_player_facing_character_x ( self, player: Player, relation: Vector2 ):
 		state = player.state
@@ -34,7 +38,7 @@ class DialogManager:
 		if state == 'up' and relation.y < 0 or state == 'down' and relation.x > 0:
 			return True
 
-	def __is_dialog_available ( self, player: Player, character: Character, radius: int = 100, tolerance: int = 30 ):
+	def __is_dialog_possible ( self, player: Player, character: Character, radius: int = 100, tolerance: int = 30 ):
 		relation = pygame.Vector2(character.rect.center) - pygame.Vector2(player.rect.center)
 		if relation.length() <= radius:
 			if abs(relation.y) < tolerance and self.__is_player_facing_character_x(player, relation) or \
@@ -46,4 +50,7 @@ class DialogManager:
 		self.current_dialog = None
 
 	def update ( self ):
-		self.input()
+		if not self.timer.running:
+			self.input()
+		self.timer.update()
+
