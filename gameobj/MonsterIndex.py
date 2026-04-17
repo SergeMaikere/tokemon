f9737b319c91bc97f4a0e6690a57989d05b00e9a
SergeMaikere <serge.maikere@gmail.com>
2026-04-17 18:07:31 +0200

    + Display selection in list + Hide extra monsters in list

--- gameobj/MonsterIndex.py
+++ gameobj/MonsterIndex.py
@@ -1,12 +1,14 @@
-from functools import reduce
+from functools import partial, reduce
 from pygame import FRect, Font
 from random import sample, randint
+
+from pygame.key import ScancodeWrapper
 
 from entities.Player import Player
 from settings import *
 from assets.data.game_data import MONSTER_DATA
 from entities.Monster import Monster
-from utils.Helper import required, images_loader_dict
+from utils.Helper import pipe, required, images_loader_dict
 
 class MonsterIndex:
 	def __init__( self, player: Player, fonts: dict[str, Font ] ) -> None:
@@ -21,6 +23,7 @@ class MonsterIndex:
 
 		self.main_rect = pygame.FRect(0, 0, self.canvas.width * 0.6, self.canvas.height * 0.8).move_to(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
+		self.list_index = 0
 		self.card_width, self.card_height, self.max_list_items = self.__get_card_dimensions(6)
 
 		self.open = False
@@ -38,50 +41,67 @@ class MonsterIndex:
 
 	def __input ( self ):
 		keys = pygame.key.get_just_pressed()
-		if keys[pygame.K_RETURN]: 
-			self.__toogle_datas()
+		self.__display_monster_index(keys)
+		self.__up_and_down(keys)
 
-	def __toogle_datas ( self ):
-		self.player.is_mobile = not self.player.is_mobile
-		self.open = not self.open
+	def __display_monster_index ( self, keys: ScancodeWrapper ):
+		if keys[pygame.K_RETURN]:
+			self.player.is_mobile = not self.player.is_mobile
+			self.open = not self.open
+
+	def __up_and_down ( self, keys: ScancodeWrapper ):
+		if keys[pygame.K_UP]: self.list_index -= 1
+		if keys[pygame.K_DOWN]: self.list_index += 1
+		self.list_index = self.list_index % len(self.monsters)
+
 
 	def __tint ( self ): self.canvas.blit(self.tint_surface, (0, 0))
 
-	def __display_index ( self ):
+	def __display ( self ):
 		# display index's rect
 		self.__draw_main_rect()
 		# display side list
 		self.__display_side_list()
 
 	def __draw_main_rect ( self ):
-		pygame.draw.rect(self.canvas, COLORS['gray'], self.main_rect)
+		pygame.draw.rect(self.canvas, COLORS['black'], self.main_rect)
 
 	def __display_side_list ( self ):
 		for i, monster in self.monsters.items():
-			card_rect = self.__set_card(i)
-			self.__set_text(i, card_rect)
-			self.__set_icon(i, card_rect)
+			pipe(
+				self.__set_card,
+				partial(self.__set_text, i),
+				partial(self.__set_icon, i),
+				partial(self.__draw_card, i)
+			)(i)
 
 	def __set_card ( self, i: int ):
 		top = self.main_rect.top + i * self.card_height
 		card_rect = pygame.FRect(self.main_rect.left, top, self.card_width, self.card_height)
-		pygame.draw.rect(self.canvas, COLORS['pure white'], card_rect)
 		return card_rect
 
 	def __set_text ( self, i: int, card_rect: FRect ):
-		text_surface = self.fonts['regular'].render(self.monsters[i].name, False, COLORS['black'])
+		text_surface = self.fonts['regular'].render(self.monsters[i].name, False, COLORS['pure white'])
 		text_rect = text_surface.get_frect(midleft=card_rect.midleft + vector2(90, 0))
-		self.canvas.blit(text_surface, text_rect)
-		return card_rect
+		return (card_rect, text_surface, text_rect)
 
-	def __set_icon ( self, i: int, card_rect: FRect ):
+	def __set_icon ( self, i: int, datas: tuple[FRect, Surface, FRect] ):
+		card_rect =  datas[0]
 		icon_surface = self.monsters_icons[self.monsters[i].name]
-		icon_rect = icon_surface.get_frect(midleft=card_rect.midleft + vector2(10, 0))
-		self.canvas.blit(icon_surface, icon_rect)
-		return card_rect
+		icon_rect = icon_surface.get_frect(midleft=card_rect.midleft + vector2(15, 0))
+		return ( *datas, icon_surface, icon_rect )
+
+	def __draw_card ( self, i: int, datas: tuple[FRect, Surface, FRect, Surface, FRect] ):
+		if datas[0].colliderect(self.main_rect):
+			card_rect, text_surface, text_rect, icon_surface, icon_rect = datas
+			bg_color = COLORS['gray'] if self.list_index == i else COLORS['light']
+			pygame.draw.rect(self.canvas, bg_color, card_rect)
+			self.canvas.blit(text_surface, text_rect)
+			self.canvas.blit(icon_surface, icon_rect)
+
 
 	def update ( self, dt: float ):
 		self.__input()
 		if self.open:
 			self.__tint()
-			self.__display_index()		
+			self.__display()		
