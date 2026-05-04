692b7553ede45102b38439a0709db60af1b52fd1
SergeMaikere <serge.maikere@gmail.com>
2026-05-05 00:42:58 +0200

    + display xp progress bar

--- gameobj/MonsterLevelSprite.py
+++ gameobj/MonsterLevelSprite.py
@@ -4,7 +4,7 @@ from settings import *
 from entities.Monster import Monster
 from utils.MyGroup import MyGroup
 from utils.Types import Trainers
-from utils.Helper import add_color_to_surface, add_text_to_card, get_rect, get_text_surface, pipe
+from utils.Helper import add_color_to_surface, add_text_to_card, get_progress_bar, get_rect, get_text_surface, pipe
 
 
 class MonsterLevelSprite ( pygame.sprite.Sprite ):
@@ -21,6 +21,7 @@ class MonsterLevelSprite ( pygame.sprite.Sprite ):
 
 		self.image, self.rect = self.__set_image_rect()
 
+		self.xp_bar_rect = pygame.FRect(0, self.rect.height - 2, self.rect.width, 2)
 
 	def __set_image_rect (  self ) -> tuple[ Surface, FRect ]:
 		return pipe(
@@ -36,6 +37,10 @@ class MonsterLevelSprite ( pygame.sprite.Sprite ):
 		level_surface = get_text_surface(self.font, f'Lvl: {self.monster.level}')
 		return add_text_to_card(self.image, level_surface)
 
+	def __display_xp_bar ( self ):
+		get_progress_bar(self.image, self.xp_bar_rect, COLORS['white'], COLORS['black'], self.monster.xp, self.monster.level_up)
+
 	def update ( self, _ ):
 		self.image = self.__add_text()
+		self.__display_xp_bar()
 	
